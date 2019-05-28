44dd0375b350f4d75b59556014dfcd9e53b24083
Mike Wilkinson <mwilkinson@imo-online.com>
2019-05-28 14:24:46 -0500

    Display paddles and ball

--- src/boilerplate/scenes/mainScene.ts
+++ src/boilerplate/scenes/mainScene.ts
@@ -1,17 +1,23 @@
 export class MainScene extends Phaser.Scene {
-  private phaserSprite: Phaser.GameObjects.Sprite;
+    private player1: Phaser.GameObjects.Sprite;
+    private player2: Phaser.GameObjects.Sprite;
+    private ball: Phaser.GameObjects.Sprite;
 
-  constructor() {
-    super({
-      key: "MainScene"
-    });
-  }
+    constructor() {
+        super({
+            key: "MainScene"
+        });
+    }
 
-  preload(): void {
-    this.load.image("logo", "./src/boilerplate/assets/phaser.png");
-  }
+    preload(): void {
+        this.load.image("ball", "./src/boilerplate/assets/ball.jpg");
+        this.load.image("paddle", "./src/boilerplate/assets/paddle.jpg");
+    }
 
-  create(): void {
-    this.phaserSprite = this.add.sprite(400, 300, "logo");
-  }
+    create(): void {
+        this.physics.world
+        this.player1 = this.add.sprite(400, 300, "paddle");
+        this.player2 = this.add.sprite(100, 300, 'paddle');
+        this.ball = this.add.sprite(250, 300, 'ball');
+    }
 }
