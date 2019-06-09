3163de2a39ca3899570489cf2aed075c1df93cfc
Mike Wilkinson <mwilkinson@imo-online.com>
2019-06-09 16:51:26 -0500

    style connection page

--- src/client/connect-view.ts
+++ src/client/connect-view.ts
@@ -4,7 +4,8 @@ export class ConnectView extends OverlayElement {
     private roomNameElement: HTMLInputElement;
     private roomJoinElement: HTMLButtonElement;
     private joinRoom: (room: string) => void;
-    private spinner: HTMLDivElement;    
+    private spinner: HTMLDivElement;
+    private errorElement: HTMLParagraphElement;
 
     constructor(joinRoom: (room: string) => void) {
         super('clientConnect');
@@ -12,8 +13,9 @@ export class ConnectView extends OverlayElement {
         this.roomNameElement = document.getElementById('roomName') as HTMLInputElement;
         this.roomJoinElement = document.getElementById('roomJoin') as HTMLButtonElement;
         this.spinner = document.getElementById('circle-loader') as HTMLDivElement;
+        this.errorElement = document.getElementById('errorMessage') as HTMLParagraphElement;
         this.roomJoinElement.addEventListener('click', () => {
-            this.roomJoinElement.disabled = true;
+            this.errorMessage = null;
             this.joinRoom(this.roomName);
         });
     }
@@ -22,10 +24,21 @@ export class ConnectView extends OverlayElement {
         return this.roomNameElement.value;
     }
 
+    public set errorMessage(value: string) {
+        this.errorElement.textContent = value;
+        if (value) {
+            this.errorElement.classList.remove('hidden');
+        } else {
+            this.errorElement.classList.add('hidden');
+        }
+    }
+
     public set isLoading(value: boolean) {
         if (value === true) {
+            this.roomJoinElement.disabled = true;
             this.spinner.classList.remove('hidden');
         } else {
+            this.roomJoinElement.disabled = false;
             this.spinner.classList.add('hidden');
         }
     }
